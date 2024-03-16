c0112edc37b19399d992e51b7fe598823539cffe
Roger-Melo <rogerw592@gmail.com>
2024-03-16 17:30:48 -0300

    ✅ Create useQuiz custom hook

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useReducer, useCallback } from 'react'
 import { Header } from '@/components/header'
 import { Timer } from '@/components/timer'
 import { Start } from '@/components/start'
@@ -6,66 +5,19 @@ import { Result } from '@/components/result'
 import { ButtonNext } from '@/components/button-next'
 import { Progress } from '@/components/progress'
 import { Questions } from '@/components/questions'
-
-const reducer = (state, action) => {
-  if (action.type === 'set_api_data') {
-    return { ...state, apiData: action.apiData }
-  }
-
-  if (action.type === 'clicked_some_option') {
-    return {
-      ...state,
-      clickedOption: action.index,
-      userScore: action.index === state.apiData[state.currentQuestion].correctOption
-        ? state.userScore + state.apiData[state.currentQuestion].points
-        : state.userScore
-    }
-  }
-
-  if (action.type === 'clicked_next_question') {
-    const wasLastQuestion = state.currentQuestion + 1 === state.apiData.length
-    return {
-      ...state,
-      currentQuestion: wasLastQuestion ? 0 : state.currentQuestion + 1,
-      clickedOption: null,
-      appStatus: wasLastQuestion ? 'finished' : state.appStatus
-    }
-  }
-
-  if (action.type === 'clicked_restart') {
-    return { ...state, userScore: 0, appStatus: 'ready', currentQuestion: 0, clickedOption: null }
-  }
-
-  if (action.type === 'clicked_start') {
-    return { ...state, appStatus: 'active' }
-  }
-
-  if (action.type === 'game_over') {
-    return { ...state, appStatus: 'finished' }
-  }
-
-  return state
-}
-
-const initialState = { currentQuestion: 0, apiData: [], clickedOption: null, userScore: 0, appStatus: 'ready' }
+import { useQuiz } from '@/hooks/quiz'
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState)
-
-  useEffect(() => {
-    fetch('https://raw.githubusercontent.com/Roger-Melo/fake-data/main/videogame-questions.json')
-      .then(response => response.json())
-      .then(apiData => dispatch({ type: 'set_api_data', apiData }))
-      .catch(error => alert(error.message))
-  }, [])
-
-  const handleClickStart = () => dispatch({ type: 'clicked_start' })
-  const handleClickOption = index => dispatch({ type: 'clicked_some_option', index })
-  const handleClickNextQuestion = () => dispatch({ type: 'clicked_next_question' })
-  const handleClickRestart = () => dispatch({ type: 'clicked_restart' })
-  const handleTimer = useCallback(({ message }) => dispatch({ type: message }), [])
-  const userHasAnswered = state.clickedOption !== null
-  const maxScore = state.apiData.reduce((acc, q) => acc + q.points, 0)
+  const {
+    state,
+    userHasAnswered,
+    maxScore,
+    handleClickStart,
+    handleClickOption,
+    handleClickNextQuestion,
+    handleClickRestart,
+    handleTimer
+  } = useQuiz()
 
   return (
     <div className="app">
