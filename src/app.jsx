6043fb495c9dadee983b3017d8df65d3c748ead8
Roger-Melo <rogerw592@gmail.com>
2024-03-16 20:27:18 -0300

    💅 Create global style

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+import { createGlobalStyle } from 'styled-components'
 import { Header } from '@/components/header'
 import { Timer } from '@/components/timer'
 import { Start } from '@/components/start'
@@ -20,24 +21,47 @@ const App = () => {
   } = useQuiz()
 
   return (
-    <div className="app">
-      <Header />
-      <main className="main">
-        {state.appStatus === 'ready' && <Start state={state} onClickStart={handleClickStart} />}
-        {state.appStatus === 'finished' && <Result state={state} maxScore={maxScore} onClickRestart={handleClickRestart} />}
-        {state.appStatus === 'active' && state.apiData.length > 0 && (
-          <>
-            <Progress state={state} maxScore={maxScore} userHasAnswered={userHasAnswered} />
-            <Questions state={state} userHasAnswered={userHasAnswered} onClickOption={handleClickOption} />
-            <div>
-              <Timer state={state} onHandleTimer={handleTimer} />
-              {userHasAnswered && <ButtonNext state={state} onClickNextQuestion={handleClickNextQuestion} />}
-            </div>
-          </>
-        )}
-      </main>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="app">
+        <Header />
+        <main className="main">
+          {state.appStatus === 'ready' && <Start state={state} onClickStart={handleClickStart} />}
+          {state.appStatus === 'finished' && <Result state={state} maxScore={maxScore} onClickRestart={handleClickRestart} />}
+          {state.appStatus === 'active' && state.apiData.length > 0 && (
+            <>
+              <Progress state={state} maxScore={maxScore} userHasAnswered={userHasAnswered} />
+              <Questions state={state} userHasAnswered={userHasAnswered} onClickOption={handleClickOption} />
+              <div>
+                <Timer state={state} onHandleTimer={handleTimer} />
+                {userHasAnswered && <ButtonNext state={state} onClickNextQuestion={handleClickNextQuestion} />}
+              </div>
+            </>
+          )}
+        </main>
+      </div>
+    </>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+
+  body {
+    min-height: 100vh;
+    color: var(--color-dark);
+    background-color: var(--color-lightest);
+    padding: 3.2rem;
+  }
+`
 
 export { App }
