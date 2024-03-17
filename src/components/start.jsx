9bbd9afded89b0d00f92094edbeeb280f1eeaae6
Roger-Melo <rogerw592@gmail.com>
2024-03-16 21:38:06 -0300

    💅 Start Styled

--- src/components/start.jsx
+++ src/components/start.jsx
@@ -1,8 +1,16 @@
+import styled from 'styled-components'
+
 const Start = ({ state, onClickStart }) =>
-  <div className="start">
+  <StyledStart>
     <h2>Bem vindo(a) ao Quiz dos Videogames!</h2>
     <h3>{state.apiData.length} questões pra te testar</h3>
     <button onClick={onClickStart} className="btn">Bora começar</button>
-  </div>
+  </StyledStart>
+
+const StyledStart = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 export { Start }
