bc4622b6776cea5571b7f62224c29d4cc17ba9ce
Roger-Melo <rogerw592@gmail.com>
2024-03-18 15:00:31 -0300

    💅 Start's titles Styled

--- src/components/start.jsx
+++ src/components/start.jsx
@@ -3,8 +3,8 @@ import { Button } from '@/components/shared/button'
 
 const Start = ({ state, onClickStart }) =>
   <StyledStart>
-    <h2>Bem vindo(a) ao Quiz dos Videogames!</h2>
-    <h3>{state.apiData.length} questões pra te testar</h3>
+    <H2>Bem vindo(a) ao Quiz dos Videogames!</H2>
+    <H3>{state.apiData.length} questões pra te testar</H3>
     <StartButton onClick={onClickStart}>Bora começar</StartButton>
   </StyledStart>
 
@@ -12,6 +12,18 @@ const StyledStart = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`
+
+const H2 = styled.h2`
+  font-size: 3.6rem;
+  margin-bottom: 2rem;
+  text-align: center;
+`
+
+const H3 = styled.h3`
+  font-size: 2.4rem;
+  font-weight: 600;
+  margin-bottom: 4rem;
 `
 
 const StartButton = styled(Button)``
