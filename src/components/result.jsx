6b518942040a392a9862568fa8ef17530dd12a51
Roger-Melo <rogerw592@gmail.com>
2024-03-18 14:16:13 -0300

    ✅💅 Create (& styled) Message Component

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -6,9 +6,7 @@ const Result = ({ state, maxScore, onClickRestart }) => {
   const resultMessage = getResultMessage({ score: state.userScore, maxScore })
   return (
     <>
-      <div className="result">
-        <span>{resultMessage}</span>
-      </div>
+      <Message>{resultMessage}</Message>
       <RestartButton onClick={onClickRestart}>Reiniciar quiz</RestartButton>
     </>
   )
@@ -16,6 +14,17 @@ const Result = ({ state, maxScore, onClickRestart }) => {
 
 const RestartButton = styled(Button)`
   float: right;
+`
+
+const Message = styled.div`
+  background-color: var(--color-theme);
+  color: var(--color-dark);
+  border-radius: 10rem;
+  text-align: center;
+  padding: 2rem 0;
+  font-size: 2.2rem;
+  font-weight: 500;
+  margin-bottom: 1.6rem;
 `
 
 export { Result }
